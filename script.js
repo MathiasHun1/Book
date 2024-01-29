01b2e908e2ed582986557d98275a5ee24816625c
MathiasHun <mathias.hun@gmail.com>
2024-01-29 19:18:51 +0100

    create a 'Book' constructor

---
+++ script.js
@@ -0,0 +1,14 @@
+function Book(title, author, isRead) {
+    this.title = title;
+    this.author = author;
+    this.isRead = isRead;
+}
+
+Book.prototype.info = function() {    
+    let read = '';
+    (this.isRead === true) ? read = 'read' : read ='not read';
+    return `The Book's title is ${this.title}, written by ${this.author}, and I have ${read} it`;
+}
+
+const cujo = new Book('Cujo', 'Stephen King', false);
+console.log(cujo.info())
