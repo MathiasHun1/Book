36c40a3e49125767dca613fcbb4a75c7f911223b
MathiasHun <mathias.hun@gmail.com>
2024-01-31 11:09:55 +0100

    added eventlistener to create new Book object, and push to an array

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector(".plus");
 const closeButton = document.querySelector("dialog button");
+const bookTitle = document.querySelector("#title");
+const bookAuthor = document.querySelector("#author");
+const bookPages = document.querySelector("#pages");
+const isRead = document.querySelector("#read");
+
+const booksArr =  [];
 
 // Constructor of Book
 function Book(title, author, pages, isRead) {
@@ -10,24 +16,25 @@ function Book(title, author, pages, isRead) {
     this.isRead = isRead;
 }
 
-Book.prototype.info = function() {    
-    let read = '';
-    (this.isRead === true) ? read = 'read' : read ='not read';
-    return `The Book's title is ${this.title}, written by ${this.author}, and I have ${read} it`;
-}
-
-const booksArr =  [];
-function addBookToLibrary() {
-    //
-}
-
-// Open and close the dialog
 showButton.addEventListener('click', () => {
     dialog.showModal();
 });
-closeButton.addEventListener('click', () => {
+closeButton.addEventListener('click', addBookToArr)
+
+
+function addBookToArr() {
+    booksArr.push(new Book(bookTitle.value, bookAuthor.value, bookPages.value, isRead.checked));
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    isRead.checked = false;
     dialog.close();
-});
+    console.log(booksArr)
+}
+
+function addBookToLibrary() {
+    
+}
 
 
 
