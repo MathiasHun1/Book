27a19607f664441f147cc989b122c139c511d86d
MathiasHun <mathias.hun@gmail.com>
2024-01-31 10:13:57 +0100

    made the feature to open and close the dialog, and blur the background when opened

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+const dialog = document.querySelector("dialog");
+const showButton = document.querySelector(".plus");
+const closeButton = document.querySelector("dialog button");
 
 // Constructor of Book
-function Book(title, author, isRead) {
+function Book(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
+    this.pages = pages;
     this.isRead = isRead;
 }
 
@@ -12,11 +16,20 @@ Book.prototype.info = function() {
     return `The Book's title is ${this.title}, written by ${this.author}, and I have ${read} it`;
 }
 
-//
-
 const booksArr =  [];
 function addBookToLibrary() {
     //
 }
+
+// Open and close the dialog
+showButton.addEventListener('click', () => {
+    dialog.showModal();
+});
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
+
+
+
 
 
